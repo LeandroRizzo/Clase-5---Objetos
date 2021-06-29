b7dc2bf0de3808aed70664a17de4f2bf15796ac3
Leandro Rizzo <86168414+LeandroRizzo@users.noreply.github.com>
2021-06-29 14:21:41 -0300

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,22 @@
+
+class Producto{
+    constructor(tipo,marca,tamaño,precio){
+        this.tipo = tipo,
+        this.marca = marca,
+        this.tamaño = tamaño,
+        this.precio = precio
+    }
+    mostrar(){
+        console.log(`${this.tipo} ${this.marca} de ${this.tamaño} tiene un valor de $ ${this.precio}`)
+    }
+}
+
+const Producto1 = new Producto("Yerba mate", "Taraguí","1 kg", 500)
+const Producto2 = new Producto("Azucar", "Ledezma","1 kg", 250)
+
+console.log(Producto1);
+console.log(Producto2);
+
+let suma = Producto1.precio + Producto2.precio
+
+console.log(`El total de la compra es de $${suma}`);
